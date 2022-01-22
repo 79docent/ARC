ead70ccc831cd7a2b72dee95d55a21e26af5210c
sjakobiak <s20574@pjwstk.edu.pl>
2022-01-22 19:34:57 +0100

    added cloud services and backend elements

---
+++ server.js
@@ -0,0 +1,92 @@
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const express = require("express");
+const admin = require("firebase-admin");
+const fs = require("fs");
+const path = require('path');
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://arc1-ba128-default-rtdb.europe-west1.firebasedatabase.app",
+});
+
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.engine("html", require("ejs").renderFile);
+app.use(express.static("static"));
+
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use(bodyParser.urlencoded({ // dodane przy dodawaniu tts
+	extended: false
+}));
+
+
+
+app.get("/login", function (req, res) {
+  res.render("login.html");
+});
+
+app.get("/signup", function (req, res) {
+  res.render("signup.html");
+});
+
+app.get("/profile", function (req, res) {
+  const sessionCookie = req.cookies.session || "";
+
+  admin
+    .auth()
+    .verifySessionCookie(sessionCookie, true /** checkRevoked */)
+    .then((userData) => {
+      console.log("Logged in:", userData.email)
+      res.render("profile.html");
+    })
+    .catch((error) => {
+      res.redirect("/login");
+    });
+});
+
+app.get("/", function (req, res) {
+  res.render("index.html");
+});
+
+app.post("/sessionLogin", (req, res) => {
+  const idToken = req.body.idToken.toString();
+
+  const expiresIn = 60 * 60 * 24 * 5 * 1000; // cookie expire time = 5 days
+
+  admin
+    .auth()
+    .createSessionCookie(idToken, { expiresIn })
+    .then(
+      (sessionCookie) => {
+        const options = { maxAge: expiresIn, httpOnly: true };
+        res.cookie("session", sessionCookie, options);
+        res.end(JSON.stringify({ status: "success" }));
+      },
+      (error) => {
+        res.status(401).send("UNAUTHORIZED REQUEST!");
+      }
+    );
+});
+
+app.get("/wrongdata", function (req, res) {
+  res.render("wrongdata.html");
+});
+
+
+app.get("/sessionLogout", (req, res) => {
+  res.clearCookie("session");
+  res.redirect("/login");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
---
+++ static/upload.js
@@ -0,0 +1,101 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-app.js";
+  
+const firebaseConfig = {
+    apiKey: "AIzaSyAuIQlwOlXD1-fLeXSFrnopctzo4rhjL1I",
+    authDomain: "arc1-ba128.firebaseapp.com",
+    databaseURL: "https://arc1-ba128-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "arc1-ba128",
+    storageBucket: "arc1-ba128.appspot.com",
+    messagingSenderId: "662160588737",
+    appId: "1:662160588737:web:4b7aa168d3852d30727940"
+  };
+
+  
+  const app = initializeApp(firebaseConfig);
+
+  import {getStorage,ref as sRef,uploadBytesResumable,getDownloadURL} from "https://www.gstatic.com/firebasejs/9.6.3/firebase-storage.js"
+ 
+
+  var files = [];
+  var reader = new FileReader();
+
+  var uploadTxtToAppButton = document.getElementById("uploadTxtToAppButton");
+  var uploadTxtToStorageButton = document.getElementById("uploadTxtToStorageButton");
+  var getTxtFromStorageButton = document.getElementById("getTxtfromStorageButton");
+  var myTextField = document.getElementById("myTextField");
+
+  uploadTxtToAppButton.onchange = e =>{
+      files = e.target.files;
+
+      var extention = getFileExt(files[0]);
+      var name = getFileName(files[0]);
+
+      reader.readAsDataURL(files[0]);
+  }
+
+  reader.onload = function(){
+      myTextField.src = reader.result;
+  }
+
+function getFileExt(file){
+    var temp = file.name.split('.');
+    var ext = temp.slice((temp.length-1),(temp.length));
+    return '.' + ext[0];
+}
+
+function getFileName(file){
+    var temp = file.name.split('.');
+    var fname = temp.slice(0,-1).join('.');
+    return fname;
+}
+
+async function UploadProcess(){
+    var txtFileToUpload = files[0];
+    var txtFileName = uploadTxtToAppButton.value;
+
+    const metaData = {
+        contentType: txtFileToUpload.type
+    }
+
+    const storage = getStorage();
+
+    const storageRef = sRef(storage, "txtFiles/"+txtFileName);
+
+    const uploadTask = uploadBytesResumable(storageRef,txtFileToUpload,metaData);
+
+    uploadTask.on("state-changed",(snapshot)=>{
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    },
+    ()=>{
+        getDownloadURL(UploadTask.snapshot.ref).then((downloadURL)=>{
+            SaveURLtoRealtimeDB(downloadURL);
+        });
+    }
+    );
+}
+
+
+//-----------------FUNKCJE BAZY-------------------//
+
+function SaveURLtoRealtimeDB(URL){
+    var txtFileName = uploadTxtToAppButton.value;
+
+    set(ref(realdb, "TxtLinks/"+txtFileName),{
+        TxtName: (txtFileName),
+        TxtURL: URL
+    })
+}
+
+// function GetURLFromRealtimeDB(){
+//     var txtFilename = uploadTxtToAppButton.value;
+//     var dbRef = ref(realdb);
+
+//     get(child(dbRef, "TxtLinks/"+txtFilename)).then((snapshot)=>{
+//         if(snapshot.exists()){
+//             myTextField.src = snapshot.val().TxtURL;
+//         }
+//     })
+// }
+
+uploadTxtToStorageButton.onclick = UploadProcess;
+// getTxtFromStorageButton.onclick = GetURLFromRealtimeDB;
